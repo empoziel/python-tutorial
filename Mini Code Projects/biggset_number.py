8a8c977ac69bcc291907ba7c52f456df0cfca07b
hasan kaplan <93055262+empoziel@users.noreply.github.com>
2021-11-21 01:05:36 +0300

    update

---
+++ Mini Code Projects/biggset_number.py
@@ -0,0 +1,10 @@
+a = int(input("first number:"))
+b = int(input("second number"))
+c = int(input("third number:"))
+
+if (a>b and a>c):
+    print("a is biggset number")
+elif (b>a and b>c):
+    print("b is biggset number")
+elif(c>a and c>b):
+    print("c is biggset number")
